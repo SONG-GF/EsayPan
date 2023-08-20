67741cf291655aa672727f8ac6a41620c6e13fb1
WithRao <sgf1228@163.com>
2023-08-20 21:03:02 +0800

    登陆页面完成90%

--- EasyPan/src/main.js
+++ EasyPan/src/main.js
@@ -1,16 +1,30 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
 //图标 图标在附件中
 import "@/assets/icon/iconfont.css";
-// import "@/assets/base.scss";
+import "@/assets/base.scss";
+
+// 引入cookies
+import VueCookies from "vue-cookies";
+import Verify from "@/utils/Verify";
+import Message from "@/utils/Message";
+import Request from "@/utils/Request";
+
+// 自定义组件
+import Dialog from "@/components/Dialog.vue";
 
 const app = createApp(App);
 app.use(ElementPlus);
-
 app.use(router);
+app.component("Dialog", Dialog);
 
-app.mount("#app");
+// 配置全局组件
+app.config.globalProperties.VueCookies = VueCookies;
+app.config.globalProperties.Verify = Verify;
+app.config.globalProperties.Message = Message;
+app.config.globalProperties.Request = Request;
+app.config.globalProperties.message = app.mount("#app");
--- EasyPan/src/router/index.js
+++ EasyPan/src/router/index.js
@@ -4,9 +4,14 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/test",
-      name: "Test",
-      component: () => import("@/views/Test.vue"),
+      path: "/login",
+      name: "Login",
+      component: () => import("@/views/Login.vue"),
+    },
+    {
+      path: "/",
+      name: "Page",
+      component: () => import("@/views/Login.vue"),
     },
   ],
 });
